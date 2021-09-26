307c80107e54553190f331bfa782ceda25e821e3
AlexMarchyk <alexmarcyk@gmail.com>
2021-09-26 16:03:39 +0300

    add getResource function, modify MenuCard

--- js/script.js
+++ js/script.js
@@ -193,38 +193,26 @@ window.addEventListener('DOMContentLoaded',()=> {
         }
 
     }
+    //  function  getResource from db.json  
+    const getResource = async (url) =>{
+        const res = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        'menu__item',
-        'tets'
-    ).render();
+        if(!res.ok){
+           throw new Error(`Could not fetch ${url}, status:${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "vegy",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        5,
-        '.menu .container',
-        'menu__item',
-    ).render();
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "vegy",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        12,
-        '.menu .container',
-        'menu__item',
-    ).render();
+            return await res.json();
+        };
 
+        getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price})=>{
+                new MenuCard(img, altimg, title, descr, price,'.menu .container').render();
+            });
+        });
+
+    
 
 // ========================FORMS====================================
 
@@ -235,10 +223,23 @@ const message ={
     failure:"Smtms wrong"
 }
 forms.forEach(item=>{
-    postData(item);
+    bindPostData(item);
 });
 
-function postData (form){
+const postData = async (url, data) =>{
+const res = await fetch(url, {
+    method:'POST',
+    headers:{
+        'Content-type':'application/json'
+    },
+    body:data
+
+});
+    return await res.json();
+};
+
+
+function bindPostData (form){
     form.addEventListener('submit',(e)=>{
         e.preventDefault();
 
@@ -247,6 +248,7 @@ function postData (form){
         statusMessage.style.cssText =`
         display:block;
         margin:0 auto;`
+        ;
         // form.append(statusMessage);
         
         form.insertAdjacentElement('afterend',statusMessage);
@@ -255,20 +257,11 @@ function postData (form){
         const formData = new FormData(form);
         //Formdata on JSON
 
-        const object = {};
-        formData.forEach(function(value, key){
-            object[key] = value;
-        });
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
       
 
-        fetch('server.php',{
-            method:'POST',
-            headers:{
-                'Content-type':'application/json'
-            },
-            body:JSON.stringify(object)
-        }).then(data => data.text())
+        postData('http://localhost:3000/requests',json)
         .then(data=>{
             console.log(data);
             showThanksModal(message.succes);
@@ -310,7 +303,7 @@ function showThanksModal(message){
         closeModal();
     },4000);
 }
-fetch('db.json')
+fetch(' http://localhost:3000/menu')
     .then(data=>data.json())
     .then(res => console.log(res));
 
