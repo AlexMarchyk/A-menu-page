acab2600d83688a4a3784b94f9e50c421cac68c7
AlexMarchyk <alexmarcyk@gmail.com>
2021-09-27 18:38:59 +0300

    add axios

--- js/script.js
+++ js/script.js
@@ -205,13 +205,20 @@ window.addEventListener('DOMContentLoaded',()=> {
             return await res.json();
         };
 
-        getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price})=>{
-                new MenuCard(img, altimg, title, descr, price,'.menu .container').render();
-            });
-        });
+        // getResource('http://localhost:3000/menu')
+        // .then(data => {
+        //     data.forEach(({img, altimg, title, descr, price})=>{
+        //         new MenuCard(img, altimg, title, descr, price,'.menu .container').render();
+        //     });
+        // });
 
+        // ========AXIOS
+        axios.get('http://localhost:3000/menu')
+        .then(data =>{
+                data.data.forEach(({img, altimg, title, descr, price})=>{
+                   new MenuCard(img, altimg, title, descr, price,'.menu .container').render();
+               });
+        });
     
 
 // ========================FORMS====================================
